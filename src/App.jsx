08bcef9668831ae4f3e23d3a9ec0511424b29c60
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-08-11 13:52:55 +0300

    Add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <AboutUs />
+      <ContactUs />
+    </>
+  );
 }
 
 export default App;
